027fe0ae0bf4facc7c5e379da446745731423dfd
RyOkEeeesh <169122209+RyOkEeeesh@users.noreply.github.com>
2024-06-18 23:59:40 +0900

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+		$('#slide-slick').slick({
+			autoplay:true,
+			autoplaySpeed:4000,
+			slidesToShow:1,
+			slidesToScroll:1,
+			arrows:false,
+			infinite:true,
+			dots:false,
+			speed:1500,
+			fade:true,
+		});
+	});
